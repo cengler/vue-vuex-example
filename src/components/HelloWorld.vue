7f2c62eb6cfe1b81a7675fa41ee3787cd78a3d89
christian.engler <christian.engler@despegar.com>
2018-09-18 22:16:42 -0300

    Simple counter app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>{{ this.count }}</div>
+    <button @click="increment()" >Up counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
